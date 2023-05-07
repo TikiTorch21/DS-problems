2065a7ef17f06ba388acfb93199fc974aea83649
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-07 14:42:39 -0400

    updated sol1.py

--- Solutions/sol1.py
+++ Solutions/sol1.py
@@ -1,27 +1,17 @@
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 def phone_num_a1(nums):
-
-	# Using for loops
-
-	phone_num = "("
-
-	for num in range(3):
-		phone_num += str(nums[num])
-	phone_num += ") "
-
-	for num in range(4, 7):
-		phone_num += str(nums[num])
-	phone_num += "-"
-
-	for num in range(7, 10):
-		phone_num += str(nums[num])
-
-	return phone_num
+	n = ''.join(map(str,nums))
+	return f"({n[:3]}) {n[3:6]}-{n[6:]}"
 
 print(f"Using approach 1:\n\t{phone_num_a1(nums)}")
 
-def phone_num_a2(nums):
+def phone_num_a2(n):
+	return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+
+print(f"Using approach 2: \n\t{phone_num_a2(nums)}")
+
+def phone_num_a3(nums):
 
 	# Using enumerate()
 
@@ -38,4 +28,4 @@ def phone_num_a2(nums):
 
 	return phone_num
 
-print(f"Using approach 2:\n\t{phone_num_a2(nums)}")
+print(f"Using approach 3:\n\t{phone_num_a2(nums)}")
