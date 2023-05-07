34a4483a317a415171b08853ebfe303c5a9da594
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-07 17:15:39 -0400

    updated sol2

--- Solutions/sol2.py
+++ Solutions/sol2.py
@@ -1,27 +1,19 @@
 nums = [2, 4, 6, 8, 10, 3]
 
-def outlier_a1(nums):
-	even_nums = []
-	odd_nums = []
+def find_outlier_a1(nums):
+	par0 = nums[0]%2
 
-	for number in nums:
-		if number % 2 == 0:
-			even_nums.append(number)
-		else:
-			odd_nums.append(number)
+	for x in nums:
+		if x%2!=par0:
+			return x
+print(f"Using approach 1:\n\t{find_outlier_a1(nums)}")
 
-	if len(odd_nums) == 1:
-		return odd_nums[0]
-	return even_nums[0]
-
-print(f"Using approach 1:\n\t{outlier_a1(nums)}")
-
-def outlier_a2(nums):
-	even_nums = list(filter(lambda x: x%2 == 0, nums))
-	odd_nums = list(filter(lambda x: x%2 != 0, nums))
+def find_outlier_a2(nums):
+	even_nums = list(filter(lambda x: x%2==0, nums))
+	odd_nums = list(filter(lambda x: x%2!=0, nums))
 
 	if len(odd_nums) == 1:
 		return odd_nums[0]
 	return even_nums[0]
 
-print(f"Using approach 2:\n\t{outlier_a2(nums)}")
+print(f"Using approach 2:\n\t{find_outlier_a2(nums)}")
