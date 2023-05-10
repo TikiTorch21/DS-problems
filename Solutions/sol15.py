9c1f147633bc27fb8612f160aaf7d4be74c27f45
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-09 20:26:10 -0400

    finished sol15

---
+++ Solutions/sol15.py
@@ -0,0 +1,27 @@
+def check_palin_a1(s):
+
+	# Non-recursive version 
+
+	punc = [",", ".", "!", "?", ";", ":", " "]
+	new_s = ''.join([i for i in s if i not in punc])
+
+	if new_s == new_s[::-1]:
+		return "palindrome"
+
+	return "not palindrome"
+
+print(f"Using approach 1:\n\t{check_palin_a1('attpptta')}")
+
+
+def check_palin_a2(s):
+	punc = [",", ".", "!", "?", ";", ":", " "]
+	new_s = ''.join([i for i in s if i not in punc])
+
+	if len(s)<2:
+		return "palindrome"
+	if new_s[0]!=new_s[-1]:
+		return "not palindrome"
+
+	return check_palin_a2(s[1:-1])
+
+print(f"Using approach 2:\n\t{check_palin_a2('attpptta')}")
