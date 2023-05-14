56f568e5b40d4f19fb307e79469b1bdd6ca0b3c2
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-14 15:42:23 -0400

    reformatted

--- Solutions/sol7a.py
+++ Solutions/sol7a.py
@@ -1,56 +1,57 @@
 def bin_conv_a1(d):
-	"""
-	Function to convert a
-	decimal number to a binary number. 
-	(APPROACH 1 - using in-built function)
+    """
+    Function to convert a
+    decimal number to a binary number.
+    (APPROACH 1 - using in-built function)
 
-	Parameters
-	----------
-	d : int
-		Input number
+    Parameters
+    ----------
+    d : int
+            Input number
 
-	Returns
-	-------
-	str
-		Binary version of input number.
-	"""
+    Returns
+    -------
+    str
+            Binary version of input number.
+    """
 
-	d_abs = abs(d)
+    d_abs = abs(d)
 
-	binary = int(bin(d_abs)[2:])
+    binary = int(bin(d_abs)[2:])
 
-	return binary if d>0 else -binary
+    return binary if d > 0 else -binary
+
 
 print(f"Using approach 1:\n\t{bin_conv_a1(-356)}")
 
 
 def bin_conv_a2(d):
+    """
+    Function to convert a
+    decimal number to a binary number.
+    (APPROACH 2 - manually)
 
-	"""
-	Function to convert a
-	decimal number to a binary number. 
-	(APPROACH 2 - manually)
+    Parameters
+    ----------
+    d : int
+            Input number
 
-	Parameters
-	----------
-	d : int
-		Input number
+    Returns
+    -------
+    str
+            Binary version of input number.
+    """
 
-	Returns
-	-------
-	str
-		Binary version of input number.
-	"""
+    d_temp = abs(d)
+    binary = ""
 
-	d_temp = abs(d)
-	binary = ""
+    while d_temp > 0:
+        rem = d_temp % 2
+        binary = str(rem) + binary
+        d_temp //= 2
 
-	while d_temp > 0:
-		rem = d_temp%2
-		binary = str(rem) + binary
-		d_temp //=2
+    return int(binary) if d > 0 else int(binary) * -1
 
-	return int(binary) if d>0 else int(binary)*-1
 
 print(f"Using approach 2:\n\t{bin_conv_a2(-356)}")
 
--- Solutions/sol8.py
+++ Solutions/sol8.py
@@ -1,48 +1,59 @@
-board = [[5, 3, 4, 6, 7, 8, 9, 1, 2], 
-	[6, 7, 2, 1, 9, 5, 3, 4, 8],  
-	[1, 9, 8, 3, 4, 2, 5, 6, 7], 
-	[8, 5, 9, 7, 6, 1, 4, 2, 3], 
-	[4, 2, 6, 8, 5, 3, 7, 9, 1], 
-	[7, 1, 3, 9, 2, 4, 8, 5, 6], 
-	[9, 6, 1, 5, 3, 7, 2, 8, 4], 
-	[2, 8, 7, 4, 1, 9, 6, 3, 5], 
-	[3, 4, 5, 2, 8, 6, 1, 7, 9]]
+board = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]
+
 
 def valid_solution(board):
-	"""
-	Function to check for a valid 
-	sudoku solution
+    """
+    Function to check for a valid
+    sudoku solution
 
-	Parameters
-	----------
-	board : list
-		Sudoku board.
+    Parameters
+    ----------
+    board : list
+            Sudoku board.
 
-	Returns
-	-------
-	bool
-		True if all the nested functions
-		are True
-	"""
-	def verify_nums(board):
-		for seq in board:
-			for index, num in enumerate(seq):
-				if num in seq[index+1:]:
-					return False
-		return True
+    Returns
+    -------
+    bool
+            True if all the nested functions
+            are True
+    """
 
-	def board_T(board):
-		return list(map(list, zip(*board)))
+    def verify_nums(board):
+        for seq in board:
+            for index, num in enumerate(seq):
+                if num in seq[index + 1 :]:
+                    return False
+        return True
 
-	def square_cr(board):
-		# Set up coordinates
+    def board_T(board):
+        return list(map(list, zip(*board)))
 
-		indices = [(x%3, x//3) for x in range(9)]
+    def square_cr(board):
+        # Set up coordinates
 
-		boxes = [[board[out_x*3+ins_x][out_y*3+ins_y] for ins_x, ins_y in indices] for out_x, out_y in indices]
-		return boxes
+        indices = [(x % 3, x // 3) for x in range(9)]
 
-	return verify_nums(board) and verify_nums(board_T(board)) and verify_nums(square_cr(board))
+        boxes = [
+            [board[out_x * 3 + ins_x][out_y * 3 + ins_y] for ins_x, ins_y in indices]
+            for out_x, out_y in indices
+        ]
+        return boxes
+
+    return (
+        verify_nums(board)
+        and verify_nums(board_T(board))
+        and verify_nums(square_cr(board))
+    )
 
 
 print(f"Using approach 1:\n\t{valid_solution(board)}")
