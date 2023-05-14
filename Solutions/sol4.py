91a8d5c7d74f12363f52b42312481caf904620ed
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-14 15:17:15 -0400

    reformatted

--- Solutions/sol4.py
+++ Solutions/sol4.py
@@ -1,66 +1,61 @@
-recipe = {
-	"flour": 500,
-	"sugar": 200,
-	"eggs": 1
-}
+recipe = {"flour": 500, "sugar": 200, "eggs": 1}
 
-available = {
-	"flour": 1200,
-	"sugar": 1200,
-	"eggs": 5,
-	"milk": 200
-}
+available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+
 
 def cakes_a1(recipe, available):
-	"""
-	Function to find number of cakes
-	that could be baked. 
-	(APPROACH 1)
+    """
+    Function to find number of cakes
+    that could be baked.
+    (APPROACH 1)
 
-	Parameters
-	----------
-	recipe : dict
-		recipe to bake a cake
-	available : dict
-		ingredients that are available
+    Parameters
+    ----------
+    recipe : dict
+            recipe to bake a cake
+    available : dict
+            ingredients that are available
 
-	Returns
-	-------
-	int
-		Number of cakes that could be baked 
-		based on what's available. 
-	"""
-	cakes = {}
+    Returns
+    -------
+    int
+            Number of cakes that could be baked
+            based on what's available.
+    """
+    cakes = {}
 
-	for ingr in recipe:
-		if available.get(ingr):
-			cakes[ingr] = available[ingr]//recipe[ingr]
-		else:
-			return 0
-	return min(cakes.values())
+    for ingr in recipe:
+        if available.get(ingr):
+            cakes[ingr] = available[ingr] // recipe[ingr]
+        else:
+            return 0
+    return min(cakes.values())
+
 
 print(f"Using approach 1:\n\t{cakes_a1(recipe, available)}")
 
+
 def cakes_a2(recipe, available):
-	"""
-	Function to find number of cakes
-	that could be baked. 
-	(APPROACH 2)
+    """
+    Function to find number of cakes
+    that could be baked.
+    (APPROACH 2)
 
-	Parameters
-	----------
-	recipe : dict
-		recipe to bake a cake
-	available : dict
-		ingredients that are available
+    Parameters
+    ----------
+    recipe : dict
+            recipe to bake a cake
+    available : dict
+            ingredients that are available
 
-	Returns
-	-------
-	int
-		Number of cakes that could be baked 
-		based on what's available. 
-	"""
-	return int(min(available.get(k, 0)/recipe[k] for k in recipe))
+    Returns
+    -------
+    int
+            Number of cakes that could be baked
+            based on what's available.
+    """
+    return int(min(available.get(k, 0) / recipe[k] for k in recipe))
+
 
 print(f"Using approach 2: \n\t{cakes_a2(recipe, available)}")
 
--- Solutions/sol5.py
+++ Solutions/sol5.py
@@ -1,35 +1,36 @@
 def sum_dig_pow_a1(a, b):
-	"""
-	Function to find numbers in a range
-	that fulfills a specific property 
-	in the problem.
+    """
+    Function to find numbers in a range
+    that fulfills a specific property
+    in the problem.
 
-	Parameters
-	----------
-	a : int
-		Starting number.
-	b : int
-		Ending number
+    Parameters
+    ----------
+    a : int
+            Starting number.
+    b : int
+            Ending number
 
-	Returns
-	-------
-	list
-		List of numbers that fulfill the specific property
-		shown in the problem. 
-	"""
+    Returns
+    -------
+    list
+            List of numbers that fulfill the specific property
+            shown in the problem.
+    """
 
-	res = []
+    res = []
 
-	for number in range(a, b+1):
-		digits = [int(i) for i in str(number)]
-		s = 0
+    for number in range(a, b + 1):
+        digits = [int(i) for i in str(number)]
+        s = 0
 
-		for idx, val in enumerate(digits):
-			s += val ** (idx+1)
+        for idx, val in enumerate(digits):
+            s += val ** (idx + 1)
 
-		if s == number:
-			res.append(number)
-	return res
+        if s == number:
+            res.append(number)
+    return res
+
 
 print(f"Using approach 1:\n\t{sum_dig_pow_a1(1, 10)}")
 
