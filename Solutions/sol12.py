afcfb3a052c426497237e792b6c7b39c5e0da698
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-15 16:21:41 -0400

    reformatted

--- Solutions/sol12.py
+++ Solutions/sol12.py
@@ -1,26 +1,27 @@
 def total_overs(bowls):
-	"""
-	Function to take a number of bowls 
-	and calculate the number of ovals and balls
-	bowled. 
+    """
+    Function to take a number of bowls
+    and calculate the number of ovals and balls
+    bowled.
 
-	Parameters
-	----------
-	bowls : int
-		number of balls bowled
+    Parameters
+    ----------
+    bowls : int
+            number of balls bowled
 
-	Returns
-	-------
-	float
-		the number of ovals and balls in the format
-		over.balls
-	"""
-	overs = (bowls//6)
-	balls = bowls - (overs*6)
+    Returns
+    -------
+    float
+            the number of ovals and balls in the format
+            over.balls
+    """
+    overs = bowls // 6
+    balls = bowls - (overs * 6)
 
-	if balls == 0:
-		return overs
-	return float(f"{overs}.{balls}")
+    if balls == 0:
+        return overs
+    return float(f"{overs}.{balls}")
+
 
 print(f"Using approach 1:\n\t{total_overs(162)}")
 
