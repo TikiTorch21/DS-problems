a6c55150dd0416725c828307f68b4213b5f5f6f5
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-14 20:10:35 -0400

    added docstrings and solutions

--- Solutions/sol10.py
+++ Solutions/sol10.py
@@ -1,7 +1,19 @@
 def number_length_a1(num):
+	"""
+	Function that takes a number as input 
+	and returns number of digits in the number. 
+	(APPROACH 1)
 
-	# Version 1 more optimized
+	Parameters
+	----------
+	num : int
+		input number
 
+	Returns 
+	-------
+	int
+		number of digits in input num.  
+	"""
 	num = abs(num)
 	d = 0
 
@@ -19,9 +31,21 @@ def number_length_a1(num):
 print(f"Using approach 1:\n\t{number_length_a1(-23001)}")
 
 def number_length_a2(num):
+	"""
+	Function that takes a number as input 
+	and returns number of digits in the number. 
+	(APPROACH 2)
 
-	# Version 2 needs to be more optimized
+	Parameters
+	----------
+	num : int
+		input number
 
+	Returns 
+	-------
+	int
+		number of digits in input num.  
+	"""
 	count = 0
 
 	if num<=9:
@@ -36,3 +60,14 @@ def number_length_a2(num):
 		num //= 10
 	return count
 print(f"Using approach 2:\n\t{number_length_a2(20)}")
+
+#####################################################
+
+"""
+Solutions
+---------
+Using approach 1:
+	5
+Using approach 2:
+	2
+"""
