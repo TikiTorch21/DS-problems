a2d39fd7a3c35f90a89b37ff6dbd3989a2de5a11
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-14 20:11:02 -0400

    reformatted

--- Solutions/sol10.py
+++ Solutions/sol10.py
@@ -1,64 +1,67 @@
 def number_length_a1(num):
-	"""
-	Function that takes a number as input 
-	and returns number of digits in the number. 
-	(APPROACH 1)
+    """
+    Function that takes a number as input
+    and returns number of digits in the number.
+    (APPROACH 1)
 
-	Parameters
-	----------
-	num : int
-		input number
+    Parameters
+    ----------
+    num : int
+            input number
 
-	Returns 
-	-------
-	int
-		number of digits in input num.  
-	"""
-	num = abs(num)
-	d = 0
+    Returns
+    -------
+    int
+            number of digits in input num.
+    """
+    num = abs(num)
+    d = 0
 
-	if num<10:
-		return d+1
+    if num < 10:
+        return d + 1
 
-	while num>0:
-		#Keep removing digits one by one and increment d
+    while num > 0:
+        # Keep removing digits one by one and increment d
 
-		num //=10
-		d+=1
-	return d
+        num //= 10
+        d += 1
+    return d
 
 
 print(f"Using approach 1:\n\t{number_length_a1(-23001)}")
 
+
 def number_length_a2(num):
-	"""
-	Function that takes a number as input 
-	and returns number of digits in the number. 
-	(APPROACH 2)
+    """
+    Function that takes a number as input
+    and returns number of digits in the number.
+    (APPROACH 2)
 
-	Parameters
-	----------
-	num : int
-		input number
+    Parameters
+    ----------
+    num : int
+            input number
 
-	Returns 
-	-------
-	int
-		number of digits in input num.  
-	"""
-	count = 0
+    Returns
+    -------
+    int
+            number of digits in input num.
+    """
+    count = 0
 
-	if num<=9:
-		return count+1
-	if num<0:
-		while num!=0:
-			count+=1
-			num //=-10
+    if num <= 9:
+        return count + 1
+    if num < 0:
+        while num != 0:
+            count += 1
+            num //= -10
 
-	while num!=0:
-		count +=1
-		num //= 10
-	return count
+    while num != 0:
+        count += 1
+        num //= 10
+    return count
+
+
 print(f"Using approach 2:\n\t{number_length_a2(20)}")
 
 #####################################################
--- Solutions/sol9.py
+++ Solutions/sol9.py
@@ -1,23 +1,26 @@
 def anagrams(word, words):
-	"""
-	Function to find anagram of input word
-	in list of words. 
+    """
+    Function to find anagram of input word
+    in list of words.
 
-	Parameters
-	----------
-	word : str
-		input word.
-	words : list
-		list of words
+    Parameters
+    ----------
+    word : str
+            input word.
+    words : list
+            list of words
 
-	Returns:
-	list
-		List of items in the input list
-		that are anagrams of input word. 
-	"""
-	return [item for item in words if sorted(item)==sorted(word)]
+    Returns:
+    list
+            List of items in the input list
+            that are anagrams of input word.
+    """
+    return [item for item in words if sorted(item) == sorted(word)]
 
-print(f"Using approach 1:\n\t{anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])}")
+
+print(
+    f"Using approach 1:\n\t{anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])}"
+)
 
 ##################################################################################################
 
