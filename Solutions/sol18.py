f30141df656135899c7438370037e3e622fc0e70
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-18 17:26:27 -0400

    reformatted

--- Solutions/sol18.py
+++ Solutions/sol18.py
@@ -1,37 +1,37 @@
 def perimeter(shape, size):
-	"""
-	Function to find and return perimeter
-	of either a square or a circle.
-	CAN ONLY USE ARITHMETIC/COMPARISON OPERATIORS
-		- No if..else statements
-		- No dictionaries
-		- No formatting methods
+    """
+    Function to find and return perimeter
+    of either a square or a circle.
+    CAN ONLY USE ARITHMETIC/COMPARISON OPERATIORS
+            - No if..else statements
+            - No dictionaries
+            - No formatting methods
 
-	Parameters
-	----------
-	shape : str
-		Shape that code finds the perimeter of
-		s for square and c for circle
+    Parameters
+    ----------
+    shape : str
+            Shape that code finds the perimeter of
+            s for square and c for circle
 
-	size : int
-		size of the input shape, depending on which shape.
-		For example, the size of the square would be the sides,
-		but for the circle it would be the diameter. 
+    size : int
+            size of the input shape, depending on which shape.
+            For example, the size of the square would be the sides,
+            but for the circle it would be the diameter.
 
-	Returns
-	-------
-	int
-		Perimeter of input shape(square or circle)
-	"""
-	def_shape = "s"
+    Returns
+    -------
+    int
+            Perimeter of input shape(square or circle)
+    """
+    def_shape = "s"
 
-	s = int(shape==def_shape)
-	c = int(shape!=def_shape)
+    s = int(shape == def_shape)
+    c = int(shape != def_shape)
 
+    p_c, p_s = 2 * 3.14 * size, 4 * size
 
-	p_c, p_s = 2*3.14*size, 4*size
+    return c * p_c + s * p_s
 
-	return c*p_c + s*p_s
 
 print(f"Using approach 1:\n\t{perimeter('s', 7)}, {perimeter('c', 9)}")
 
--- Solutions/sol20.py
+++ Solutions/sol20.py
@@ -1,20 +1,22 @@
 d = {"z": "q", "w": "f"}
 
+
 def invert(d):
-	"""
-	Function to invert keys of input dictionary.
+    """
+    Function to invert keys of input dictionary.
 
-	Parameters
-	----------
-	d : dict
-		input dictionary
+    Parameters
+    ----------
+    d : dict
+            input dictionary
 
-	Returns
-	-------
-	dict
-		inverted version of input dictionary.
-	"""
-	return {v: k for k, v in d.items()}
+    Returns
+    -------
+    dict
+            inverted version of input dictionary.
+    """
+    return {v: k for k, v in d.items()}
+
 
 print(f"Using approach 1:\n\t{invert(d)}")
 
