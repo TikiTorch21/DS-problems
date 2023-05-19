471b5d94196e8e96617a9e7ac80b6588341fe98f
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-19 19:01:29 -0400

    reformatted

--- Solutions/sol23.py
+++ Solutions/sol23.py
@@ -1,32 +1,35 @@
 def ips_between(start, end):
-	"""
-	Function to find the number of IPS
-	adresses between input start and input end. 
+    """
+    Function to find the number of IPS
+    adresses between input start and input end.
 
-	Parameters
-	----------
-	start : str
-		input start
-	end : str
-		input end
+    Parameters
+    ----------
+    start : str
+            input start
+    end : str
+            input end
 
-	Returns
-	-------
-	int
-		number of IPS adresses between
-		input start and input end. 
-	"""
-	start = start.split(".")
-	end = end.split(".")
+    Returns
+    -------
+    int
+            number of IPS adresses between
+            input start and input end.
+    """
+    start = start.split(".")
+    end = end.split(".")
 
-	last = int(end[-1])-int(start[-1])
-	slast = (int(end[-2]) - int(start[-2]))*256
-	tlast = (int(end[-3]) - int(start[-3]))*256*256
-	first = (int(end[0]) - int(start[0])) *256*256*256
+    last = int(end[-1]) - int(start[-1])
+    slast = (int(end[-2]) - int(start[-2])) * 256
+    tlast = (int(end[-3]) - int(start[-3])) * 256 * 256
+    first = (int(end[0]) - int(start[0])) * 256 * 256 * 256
 
-	return last+slast+tlast+first
+    return last + slast + tlast + first
 
-print(f"Using approach 1:\n\t{ips_between('10.0.0.0', '10.0.0.50')}, {ips_between('20.0.0.10', '20.0.1.0')}")
+
+print(
+    f"Using approach 1:\n\t{ips_between('10.0.0.0', '10.0.0.50')}, {ips_between('20.0.0.10', '20.0.1.0')}"
+)
 
 #############################################################################################################
 
--- Solutions/sol24.py
+++ Solutions/sol24.py
@@ -1,26 +1,26 @@
 def running_sum(li):
-	"""
-	Function to find and return the running sum 
-	in an input list of numbers.
+    """
+    Function to find and return the running sum
+    in an input list of numbers.
 
-	Parameters
-	----------
-	li : list
-		input list
+    Parameters
+    ----------
+    li : list
+            input list
 
-	Returns
-	-------
-	list
-		running sum in an input list of numbers.
-	"""
-	run_sum = []
-	s = 0
-	for n in li:
-		s += n
-		run_sum.append(s)
+    Returns
+    -------
+    list
+            running sum in an input list of numbers.
+    """
+    run_sum = []
+    s = 0
+    for n in li:
+        s += n
+        run_sum.append(s)
 
+    return run_sum
 
-	return run_sum
 
 print(f"Using approach 1:\n\t{running_sum([1, 1, 1, 1])}, {running_sum([1, 2, 3, 4])}")
 
