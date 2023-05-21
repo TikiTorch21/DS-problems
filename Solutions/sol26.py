01307c3122075587ca20431f9b96f05587d14c00
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-21 19:06:52 -0400

    reformatted

--- Solutions/sol26.py
+++ Solutions/sol26.py
@@ -1,41 +1,40 @@
 def iso(s, c):
-	"""
-	Function to determine if two input strings
-	are isomorphic.
+    """
+    Function to determine if two input strings
+    are isomorphic.
 
-	Parameters
-	----------
-	s : str
-		first input string
-	c : str
-		second input string
+    Parameters
+    ----------
+    s : str
+            first input string
+    c : str
+            second input string
 
-	Returns
-	-------
-	bool
-		True or False value depending on 
-		if two input strings are isomorphic. 
-	"""
-	s_dict = {}
-	c_dict = {}
+    Returns
+    -------
+    bool
+            True or False value depending on
+            if two input strings are isomorphic.
+    """
+    s_dict = {}
+    c_dict = {}
 
-	for x in s:
-		if x in s_dict.keys():
-			s_dict[x]+=1
+    for x in s:
+        if x in s_dict.keys():
+            s_dict[x] += 1
 
-		else:
-			s_dict[x] = 1
-	for x in s:
-		if x in c_dict.keys():
-			c_dict[x]+=1
-		else:
-			c_dict[x]=1
+        else:
+            s_dict[x] = 1
+    for x in s:
+        if x in c_dict.keys():
+            c_dict[x] += 1
+        else:
+            c_dict[x] = 1
 
-	s_list = list(s_dict.values())
-	c_list = list(c_dict.values())
+    s_list = list(s_dict.values())
+    c_list = list(c_dict.values())
 
-	return s_list == c_list
-
+    return s_list == c_list
 
 
 print(f"Using approach 1:\n\t{iso('paper', 'title')}")
--- Solutions/sol27.py
+++ Solutions/sol27.py
@@ -1,70 +1,76 @@
 import numpy as np
 
+
 def identity_a1():
-	"""
-	Function to create an identity matrix
-	of order 3. 
-	(APPROACH 1 - Using .identity)
+    """
+    Function to create an identity matrix
+    of order 3.
+    (APPROACH 1 - Using .identity)
 
-	Parameters
-	----------
-	NONE
+    Parameters
+    ----------
+    NONE
 
-	Returns
-	-------
-	numpy array
-		idendity matrix of order 3
-	"""
+    Returns
+    -------
+    numpy array
+            idendity matrix of order 3
+    """
 
-	identity = np.identity(3, dtype="int")
+    identity = np.identity(3, dtype="int")
 
-	return identity
+    return identity
+
 
 print(f"Using approach 1:\n\t{identity_a1()}")
 
+
 def identity_a2():
-	"""
-	Function to create an identity matrix
-	of order 3. 
-	(APPROACH 2 - Using .eye)
+    """
+    Function to create an identity matrix
+    of order 3.
+    (APPROACH 2 - Using .eye)
 
-	Parameters
-	----------
-	NONE
+    Parameters
+    ----------
+    NONE
 
-	Returns
-	-------
-	numpy array
-		idendity matrix of order 3
-	"""
-	
-	return np.eye(3, dtype="int")
+    Returns
+    -------
+    numpy array
+            idendity matrix of order 3
+    """
+
+    return np.eye(3, dtype="int")
+
 
 print(f"Using approach 2:\n\t{identity_a2()}")
 
+
 def identity_a3():
-	"""
-	Function to create an identity matrix
-	of order 3. 
-	(APPROACH 3 - Manual approach)
+    """
+    Function to create an identity matrix
+    of order 3.
+    (APPROACH 3 - Manual approach)
 
-	Parameters
-	----------
-	NONE
+    Parameters
+    ----------
+    NONE
 
-	Returns
-	-------
-	numpy array
-		idendity matrix of order 3
-	"""
+    Returns
+    -------
+    numpy array
+            idendity matrix of order 3
+    """
 
-	m = np.zeros((3, 3), dtype="int")
+    m = np.zeros((3, 3), dtype="int")
 
-	for row in range(3):
-		for column in range(3):
-			if row == column:
-				m[row, column] = 1
-	return m
+    for row in range(3):
+        for column in range(3):
+            if row == column:
+                m[row, column] = 1
+    return m
+
 
 print(f"Using approach 3:\n\t{identity_a3()}")
 
