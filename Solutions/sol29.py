5060ca175109d5df4bae4212bba2301410d0da87
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-21 19:18:46 -0400

    added docstrings and solutions

--- Solutions/sol29.py
+++ Solutions/sol29.py
@@ -1,4 +1,19 @@
 def harshad(num):
+	"""
+	Function to check if input integer 
+	is a harshad number. 
+
+	Parameters
+	----------
+	num : int
+		input num 
+
+	Returns
+	-------
+	bool
+		True or False value depending on
+		if input num in a harshad number. 
+	"""
 	s_num = str(num)
 	temp = 0
 
@@ -8,3 +23,12 @@ def harshad(num):
 	return num % temp == 0
 
 print(f"Using approach 1:\n\t{harshad(18)}")
+
+############################################
+
+"""
+Solutions
+---------
+Using approach 1:
+	True
+"""
--- Solutions/sol30.py
+++ Solutions/sol30.py
@@ -1,6 +1,19 @@
 def solution(lis):
+	"""
+	Function to calculate the number of jumps
+	needed to jump out of the list.
 
-	# Variables
+	Parameters
+	----------
+	lis : list
+		input list
+
+	Returns
+	-------
+	int
+		number of jumps needed 
+		to jump out of the list. 
+	"""
 	pos = 1
 	visited = [lis[0]]
 	jumps = 1
@@ -16,3 +29,12 @@ def solution(lis):
 		jumps += 1
 
 print(f"Using approach 1:\n\t{solution([1, 2, 2, 9, 1, 1, 1, 1, 1, 1, 1, 2, -1])}")
+
+###################################################################################
+
+"""
+Solutions
+---------
+Using approach 1:
+	5
+"""
