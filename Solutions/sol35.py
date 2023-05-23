b6f69aadd832f8b2eb1a4620983d0cd1ea032373
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-23 16:41:10 -0400

    updated sol35

--- Solutions/sol35.py
+++ Solutions/sol35.py
@@ -1,7 +1,7 @@
 def give_change_a1(money):
     """
-    Function to break money down to 
-    smallest number of bills possible. 
+    Function to break money down to
+    smallest number of bills possible.
     (APPROACH 1 - USING DICTIONARY)
 
     Parameters
@@ -12,32 +12,27 @@ def give_change_a1(money):
     Returns
     -------
     tuple
-        Tuple of the bills the money 
-        was broken down into. 
-    
+        Tuple of the bills the money
+        was broken down into.
+
     """
-    
-    bills_dict = {
-        1:0,
-        5:0,
-        10:0,
-        20:0,
-        50:0,
-        100:0
-    }
-    
+
+    bills_dict = {1: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0}
+
     for bill in [100, 50, 20, 10, 5, 1]:
         bills_dict[bill] = money // bill
-        money -= bill*(money // bill)
-        
+        money -= bill * (money // bill)
+
     return tuple(bills_dict.values())
+
 
 print(f"Using approach 1:\n\t{give_change_a1(217)}")
 
+
 def give_change_a2(amount):
     """
-    Function to break money down to 
-    smallest number of bills possible. 
+    Function to break money down to
+    smallest number of bills possible.
     (APPROACH 2 - Using variables)
 
     Parameters
@@ -48,18 +43,21 @@ def give_change_a2(amount):
     Returns
     -------
     tuple
-        Tuple of the bills the money 
-        was broken down into. 
-    
+        Tuple of the bills the money
+        was broken down into.
+
     """
-    hundredd = amount//100
-    fiftyd = (amount - 100*hundredd)//50
-    twentyd = (amount - 100*hundredd - 50*fiftyd)//20
-    tend = (amount - 100*hundredd - 50*fiftyd - 20*twentyd)//10
-    fived = (amount - 100*hundredd - 50*fiftyd - 20*twentyd - 10*tend)//5
-    oned = (amount - 100*hundredd - 50*fiftyd - 20*twentyd - 10*tend - 5*fived)//1
-    
+    hundredd = amount // 100
+    fiftyd = (amount - 100 * hundredd) // 50
+    twentyd = (amount - 100 * hundredd - 50 * fiftyd) // 20
+    tend = (amount - 100 * hundredd - 50 * fiftyd - 20 * twentyd) // 10
+    fived = (amount - 100 * hundredd - 50 * fiftyd - 20 * twentyd - 10 * tend) // 5
+    oned = (
+        amount - 100 * hundredd - 50 * fiftyd - 20 * twentyd - 10 * tend - 5 * fived
+    ) // 1
+
     return (oned, fived, tend, twentyd, fiftyd, hundredd)
+
 
 print(f"Using approach 2:\n\t{give_change_a2(217)}")
 
