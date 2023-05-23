3f5f7ce67cf62c3fbe83c135741a5ab9c2a75c94
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-23 16:43:24 -0400

    reformatted sol36

--- Solutions/sol36.py
+++ Solutions/sol36.py
@@ -1,6 +1,6 @@
 def check_fib_count(n):
     """
-  
+
     Function to calculate the nth element in the Fibonacci sequence,
     and then count the occurence of each digit in the number.
 
@@ -13,17 +13,18 @@ def check_fib_count(n):
     -------
     list
         A list of tuples that contains the information
-        of the occurence of each digit in the number. 
+        of the occurence of each digit in the number.
 
     """
+
     def fib_rec_mem(n, fib_dict):
         if n not in fib_dict:
-            fib_dict[n] = fib_rec_mem(n-1, fib_dict) + fib_rec_mem(n-2, fib_dict)
-        
+            fib_dict[n] = fib_rec_mem(n - 1, fib_dict) + fib_rec_mem(n - 2, fib_dict)
+
         return fib_dict[n]
-    
+
     f_number = fib_rec_mem(n, {1: 0, 2: 1})
-    
+
     count_list = []
     s_fnumber = str(f_number)
 
@@ -34,7 +35,7 @@ def check_fib_count(n):
                 count += 1
         count_list.append((int(digit), count))
         s_fnumber = s_fnumber.replace(digit, "")
-    
+
     for t in count_list:
         if 0 in t:
             count_list.pop(count_list.index(t))
