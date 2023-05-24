57eecfc90937cfeb008c5e54f39b09749f7b4d77
TikiTorch21 <prateek.mannem@gmail.com>
2023-05-24 16:23:10 -0400

    reformatted sol37

--- Solutions/sol37.py
+++ Solutions/sol37.py
@@ -1,7 +1,7 @@
 def valid_ISBN10_a1(ISBN):
     """
-    Function to check if ISBN number 
-    is valid. 
+    Function to check if ISBN number
+    is valid.
     (APPROACH 1)
 
     Parameters
@@ -13,25 +13,27 @@ def valid_ISBN10_a1(ISBN):
     -------
     bool
         True or False value depending
-        on if the input string is a valid ISBN number or not. 
+        on if the input string is a valid ISBN number or not.
     """
     adding = 0
-    if len(ISBN)==10 and ISBN[:9].isdigit():
+    if len(ISBN) == 10 and ISBN[:9].isdigit():
         for count, digit in enumerate(ISBN, 1):
             if digit == "X":
-                adding += 10*10
+                adding += 10 * 10
             elif digit.isdigit():
-                adding += int(digit)*count
+                adding += int(digit) * count
         if adding % 11 == 0:
             return True
     return False
 
+
 print(f"Using approach 1:\n\t{valid_ISBN10_a1('048665088X')}")
+
 
 def valid_ISBN10_a2(isbn):
     """
-    Function to check if ISBN number 
-    is valid. 
+    Function to check if ISBN number
+    is valid.
     (APPROACH 2)
 
     Parameters
@@ -43,25 +45,26 @@ def valid_ISBN10_a2(isbn):
     -------
     bool
         True or False value depending
-        on if the input string is a valid ISBN number or not. 
+        on if the input string is a valid ISBN number or not.
     """
-    if len(isbn)==10:
+    if len(isbn) == 10:
         value = 0
         for idx, num in enumerate(isbn, start=1):
             try:
-                if num=='X' and idx==10:
+                if num == "X" and idx == 10:
                     num = 10
 
                 temp_num = int(num)
-                value = value + idx*temp_num
+                value = value + idx * temp_num
 
             except ValueError:
                 return False
 
-        if value%11==0:
+        if value % 11 == 0:
             return True
 
     return False
+
 
 print(f"Using approach 2:\n\t{valid_ISBN10_a2('048665088X')}")
 
