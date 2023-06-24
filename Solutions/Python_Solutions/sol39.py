dd0e30ff215057b47ad1e535ee939dccc756d857
TikiTorch21 <prateek.mannem@gmail.com>
2023-06-24 10:24:54 -0400

    added new solution

--- Solutions/Python_Solutions/sol39.py
+++ Solutions/Python_Solutions/sol39.py
@@ -12,7 +12,7 @@ def zero_or_one(n, array):
     Parameters
     ----------
     n : int
-        number of each list in the input array.
+        number of sublists in array.
     array : list
         input array.
 
@@ -38,11 +38,29 @@ def zero_or_one(n, array):
 print(
     f"Using approach 1(Three test cases):\n\t{zero_or_one(1, [[1,1,0,1]])}, {zero_or_one(3, [[1,0,1,0,1], [1,0,1,0,1], [0,1,0,1,0]])}, {zero_or_one(5,  [[1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1]])}"
 )
+
+"""
+Using approach 2
+"""
+
+zero_or_one_w_lambda = lambda n, s: [max(c, key=c.count) for c in zip(*s)]
+
+s = [[1,0,0,0,2], 
+      [0,1,0,0,0], 
+      [0,0,1,0,4], 
+      [0,0,0,1,0], 
+      [0,0,0,0,1]]
+
+print(
+    f"Using approach 2(Three test cases):\n\t{zero_or_one_w_lambda(1, [[1,1,0,1]])}, {zero_or_one_w_lambda(3, [[1,0,1,0,1], [1,0,1,0,1], [0,1,0,1,0]])}, {zero_or_one_w_lambda(5,  [[1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1]])}"
+)
 ###############################################################################################################################################################################################################################
 
 """
 Solutions
 ---------
 Using approach 1(Three test cases):
+        [1, 1, 0, 1], [1, 0, 1, 0, 1], [0, 0, 0, 0, 0]
+Using approach 2(Three test cases):
         [1, 1, 0, 1], [1, 0, 1, 0, 1], [0, 0, 0, 0, 0]
 """
